e6f5c749c155b481f44c48c8dd17997f94c5e4bc
MatheusHenriqueOtto <matheusotto1906@gmail.com>
2026-05-28 21:52:02 -0300

    Realizando atividades em casa

--- atividades/assets/js/exercicio-31-api-fetch-produtos.js
+++ atividades/assets/js/exercicio-31-api-fetch-produtos.js
@@ -1,6 +1,7 @@
 const url = "https://api.franciscosensaulas.com/api/v1/empresa/produtos"
 
 function criarProduto(){
+    const id = parseInt("Digite o id do produto");
     const nome = prompt("Digite o nome do produto");
     const preco = parseFloat(prompt("Digite o preço do produto"));
     const categoria = prompt("Digite a categoria");
@@ -9,7 +10,7 @@ function criarProduto(){
         id: id,
         nome: nome,
         preco: preco,
-        categori: categoria
+        categoria: categoria
     }
 
     fetch(url, {
@@ -30,6 +31,44 @@ function criarProduto(){
 }
 
 function consultarProdutos(){
-    const id = parseInt(prompt("Digite um id para o produto"));
-    const nome = prompt("Digite ")
+    const textarea = document.getElementById("tabela-mostrar");
+    textarea.value = "";
+
+    fetch(url)
+        .then(response => response.json())
+        .then(empresas => {
+            for (let i = 0; i < empresas.lenght; i += 1){
+                const empresa = empresas[i];
+                const texto = `ID: ${empresa.id} | Nome: ${empresa.nome} | Preço: ${empresa.preco} | Classificação: ${empresa.categoria}`;
+                textarea.value = textarea.value + texto;
+            }
+        })
+        .catch(error => {
+            console.error(`Erro: ${error}`);
+            alert("Falha ao mostrar todos os produtos");
+        })
+}
+
+function alterarProduto(){
+    const idParaAlterar = "/"+ ((prompt("Digite o id do produto que deseja alterar")).trim());
+    const urlDesejada = url + idParaAlterar;
+
+    const novoNome = prompt("Digite o novo nome do produto");
+    const novoPreco = parseInt(prompt("Digite um novo preço"));
+    const novaCategoria = prompt("Digite a nova categoria");
+
+    const dados = {
+        nome: novoNome,
+        preco: novoPreco,
+        categoria: novaCategoria
+    }
+
+    fetch(urlDesejada, {
+        method: "PUT",
+        headers:{
+            "Content-Type":"appliication/json"
+        },
+        body: JSON.stringify(dados)
+    })
+        .then
 }
